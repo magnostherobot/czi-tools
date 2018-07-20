5e5620bd136361b5cdc73296d6929ec80326ca4c
David Miller <sysvinit@users.noreply.github.com>
2018-07-20 16:32:14 +0100

    Memory allocation wrappers, basic mapped file open/close functionality

---
+++ czinspect/src/operations.h
@@ -0,0 +1,11 @@
+#ifndef _OPERATIONS_H
+#define _OPERATIONS_H
+
+#include "types.h"
+
+void do_scan(struct config *);
+void do_extract(struct config *);
+void do_dump(struct config *);
+
+#endif /* _OPERATIONS_H */
+
