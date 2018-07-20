61c8852ca3fb2cbf223d5ef491faf6f17ac55158
David Miller <sysvinit@users.noreply.github.com>
2018-07-20 11:06:48 +0100

    Finish off first pass option parsing and help text.
    This includes some BSD compat code from OpenBSD.

--- czinspect/src/czinspect.h
+++ czinspect/src/czinspect.h
@@ -3,10 +3,21 @@
 
 #include <stdint.h>
 
+#include "config.h"
+
 struct config {
     char *infile;
     uint16_t operation;
     uint8_t help;
+    uint8_t progress_bar;
+    
+    char *outdir;
+
+    char *outfile;
+    
+#ifdef SMALL_ADDRESS
+    size_t page_multiplier;
+#endif
 };
 
 #endif /* _CZINSPECT_H */
