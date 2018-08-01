c17b61fbc27a1d602120df61773e4a32badbfe7f
David Miller <sysvinit@users.noreply.github.com>
2018-08-01 12:15:13 +0100

    Implement subsampling ratio scanning and subsampling ratio filtering upon extraction.
    This also implements an option to round the subsampling ratio down
    to the next nearest if no exact match is found.
    
    This commit also fixes some rather upsetting memory corruption issues
    in alloc.c.

--- src/czinspect/src/util.h
+++ src/czinspect/src/util.h
@@ -4,9 +4,13 @@
 #include <stdint.h>
 
 #include "types.h"
+#include "alloc.h"
 
 #define czi_subblock_dim_ratio(d)    ((d)->stored_size == 0 ? 1 : ((d)->size / (d)->stored_size))
 
 int xfallocate(int, size_t);
+
+uint32_t get_subsample_level(lzbuf, uint32_t);
+int make_reslist(struct map_ctx *, lzbuf, uint32_t *);
 
 #endif /* _UTIL_H */
