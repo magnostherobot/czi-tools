5520698c6a72458a2243dd7680ae766a927320a0
Tom Harley <trh@st-andrews.ac.uk>
2018-08-28 21:13:18 +0100

    Fix edge case when stitching edges
    Regions involving the right- or bottom-most edges would add black bars
    between tiles if the edges' tiles were irregularly large. Handling the
    bottom and right edges of the region individually solves this.
    
    Additionally, the stitching now actually works. At least, it passes
    basic testing.

--- src/regions/region.c
+++ src/regions/region.c
@@ -106,6 +106,17 @@ czi_coord_t str_czi_coord(char* str, char **end, int base) {
     return ret;
 }
 
+/*
+ * Like strstr(3), but points to the end of the needle rather than the start.
+ */
+char *strstrend(const char *str, const char *needle) {
+    char *mid = strstr(str, needle);
+    if (!mid) {
+        return 0;
+    }
+    return mid + strlen(needle);
+}
+
 int set_side(struct dirent *ent, char *id, struct options *opts,
         czi_coord_t *left, czi_coord_t *right, int *scale) {
     char *filename = ent->d_name;
@@ -119,33 +130,41 @@ int set_side(struct dirent *ent, char *id, struct options *opts,
      * While the ordering of values in a filename has been agreed, this has
      * been written to work with values in any order, just in case.
      */
-#   define place(str, x) (strstr((str), (x)) + strlen((x)))
-    char *dim_start = place(filename,id);
+    char *dim_start = strstrend(filename,id);
+    if (!dim_start) return 1;
 
-    char *left_start = place(dim_start, "p");
+    char *left_start = strstrend(dim_start, "p");
+    if (!left_start) return 2;
     *left = str_czi_coord(left_start, NULL, base);
 
     if (right) {
-        char *size_start = place(dim_start, "s");
+        char *size_start = strstrend(dim_start, "s");
+        if (!size_start) return 3;
         *right = *left + str_czi_coord(size_start, NULL, base);
     }
 
     if (scale) {
-        char *scale_start = place(dim_start, "r");
+        char *scale_start = strstrend(dim_start, "r");
+        if (!scale_start) return 4;
+
         czi_coord_t scale_tmp = str_czi_coord(scale_start, NULL, base);
         assert(scale_tmp <= INT_MAX);
         *scale = (int) scale_tmp;
     }
 
     return 0;
-#   undef place
 }
 
 int get_region(struct dirent *ent, struct region *buf, struct options *opts) {
-    set_side(ent, "X", opts, &(buf->left), &(buf->right), &(buf->scale));
+    int ss_ret;
+
+    ss_ret = set_side(ent, "X", opts, &(buf->left), &(buf->right),
+            &(buf->scale));
+    if (ss_ret) return ss_ret;
 
     int scale_tmp;
-    set_side(ent, "Y", opts, &(buf->up),   &(buf->down),  &scale_tmp);
+    ss_ret = set_side(ent, "Y", opts, &(buf->up), &(buf->down), &scale_tmp);
+    if (ss_ret) return ss_ret;
     assert(buf->scale == scale_tmp);
 
     return 0;
@@ -170,7 +189,7 @@ llist *find_relevant_tiles(struct region *desired, char *tile_dirname,
     struct dirent *ent;
     while ((ent = readdir(dir))) {
         struct region tile_region;
-        if (!get_region(ent, &tile_region, opts)) continue;
+        if (get_region(ent, &tile_region, opts)) continue;
         if (tile_region.scale != desired->scale) continue;
         if (overlaps(&tile_region, desired)) {
             struct tile *tile = (struct tile *) malloc(sizeof (*tile));
@@ -181,8 +200,6 @@ llist *find_relevant_tiles(struct region *desired, char *tile_dirname,
     }
     return included_tiles;
 }
-
-#define safe_vips(...) if (!(__VA_ARGS__)) vips_error_exit(NULL)
 
 struct filenamedata {
     char       *start;
@@ -235,6 +252,87 @@ void debug_llist(llist *list) {
     ll_foreach(list, &print_filename, NULL);
 }
 
+int stitch_tiles(VipsImage **in, VipsImage **out, int tile_count, int across) {
+#   define safe_call(func) do { if (func) { return -1; } } while (0)
+#   define safe_arrayjoin(in, out, n, ...) \
+        safe_call(vips_arrayjoin(in, out, n, __VA_ARGS__))
+#   define safe_join(a, b, out, dir, ...) \
+        safe_call(vips_join(a, b, out, dir, __VA_ARGS__))
+
+    assert(tile_count % across == 0);
+
+    if (tile_count == 1) {
+        // There's only one tile: no need to stitch anything
+        *out = in[0];
+        return 0;
+    } else if (tile_count == across) {
+        // All the tiles are in one row
+        VipsImage *left, *right;
+
+        safe_arrayjoin(in, &left, tile_count - 1, NULL);
+        right = in[tile_count - 1];
+        safe_join(left, right, out, VIPS_DIRECTION_HORIZONTAL, "expand", true,
+                NULL);
+        return 0;
+    } else if (across == 1) {
+        // All the tiles are in one column
+        VipsImage *top, *bottom;
+
+        safe_arrayjoin(in, &top, tile_count - 1, "across", across, NULL);
+        bottom = in[tile_count - 1];
+        safe_join(top, bottom, out, VIPS_DIRECTION_VERTICAL, "expand", true,
+                NULL);
+        return 0;
+    }
+
+    // All but the last tile on a row, and the entire last row, are
+    // array-joined:
+    int basic_tile_count = tile_count - across - (tile_count / across) + 1;
+    VipsImage *basic_tiles_in[basic_tile_count];
+    VipsImage *basic_tiles_out;
+    for (int i = 0; i < basic_tile_count; ++i) {
+        basic_tiles_in[i] = in[i + ((i + 1) / across)];
+    }
+    safe_arrayjoin(basic_tiles_in, &basic_tiles_out, basic_tile_count,
+            "across", across - 1, NULL);
+
+    // Array-join the bottom row (except the bottom-right corner):
+    int bottom_row_count = across - 1;
+    VipsImage **bottom_row_in = in + tile_count - across;
+    VipsImage *bottom_row_out;
+    safe_arrayjoin(bottom_row_in, &bottom_row_out, bottom_row_count, NULL);
+
+    // Array-join the rightmost column (except the bottom-right corner):
+    int right_col_count = tile_count / across - 1;
+    VipsImage *right_col_in[right_col_count];
+    VipsImage *right_col_out;
+    for (int i = 1; i <= right_col_count; ++i) {
+        right_col_in[i - 1] = in[i * across - 1];
+    }
+    safe_arrayjoin(right_col_in, &right_col_out, right_col_count, "across", 1,
+            NULL);
+
+    // Merge the main tiles and the bottom row:
+    VipsImage *left;
+    safe_join(basic_tiles_out, bottom_row_out, &left, VIPS_DIRECTION_VERTICAL,
+            "expand", true, NULL);
+
+    // Merge the rightmost column and bottom-right corner tile:
+    VipsImage *right;
+    safe_join(right_col_out, in[tile_count - 1], &right,
+            VIPS_DIRECTION_VERTICAL, "expand", true, NULL);
+
+    // Altogether now:
+    safe_join(left, right, out, VIPS_DIRECTION_HORIZONTAL, "expand", true,
+            NULL);
+
+    return 0;
+
+#   undef safe_call
+#   undef safe_arrayjoin
+#   undef safe_join
+}
+
 void stitch_region(struct region *desired, char *tile_dirname,
         struct options *opts) {
     debug_region(desired);
@@ -247,8 +345,8 @@ void stitch_region(struct region *desired, char *tile_dirname,
         errx(1, "no tiles found for specified region");
     }
 
-    if (vips_arrayjoin(vips_in, &vips_mid, ntiles, "across",
-                tiles_across(included_tiles), NULL)) {
+    int across = tiles_across(included_tiles);
+    if (stitch_tiles(vips_in, &vips_mid, ntiles, across)) {
         vips_error_exit(NULL);
     }
 
@@ -258,7 +356,6 @@ void stitch_region(struct region *desired, char *tile_dirname,
     move_relative(&(((struct tile *) (included_tiles->content))->region), desired);
     debug_region(desired);
     debug_llist(included_tiles);
-
 
     if (vips_crop(vips_mid, &vips_out, desired->left, desired->up,
                 desired->right - desired->left, desired->down - desired->up,
